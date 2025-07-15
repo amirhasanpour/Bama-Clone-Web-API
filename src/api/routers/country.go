c7511a42f4f6f34213f9bc3174d6deaaf55ba98c
amirhp <amirhasanpour.hp10@gmail.com>
2025-07-16 01:10:28 +0330

    Create country API

---
+++ src/api/routers/country.go
@@ -0,0 +1,16 @@
+package routers
+
+import (
+	"github.com/amirhasanpour/car-sale-management-wep-api/src/api/handlers"
+	"github.com/amirhasanpour/car-sale-management-wep-api/src/config"
+	"github.com/gin-gonic/gin"
+)
+
+func Country(r *gin.RouterGroup, cfg *config.Config) {
+	h := handlers.NewCountryHandler(cfg)
+
+	r.POST("/", h.Create)
+	r.PUT("/:id", h.Update)
+	r.DELETE("/:id", h.Delete)
+	r.GET("/:id", h.GetById)
+}
