9d518d32071a4e2fa1a80c8c2bf191c306d49e50
amirhp <amirhasanpour.hp10@gmail.com>
2025-07-30 01:01:30 +0330

    Add car model endpoints

--- src/api/api.go
+++ src/api/api.go
@@ -63,6 +63,7 @@ func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
 		// Car
 		carTypes := v1.Group("/car-types", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 		gearboxes := v1.Group("/gearboxes", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
+		carModels := v1.Group("/car-models", middlewares.Authentication(cfg), middlewares.Authorization([]string{"admin"}))
 
 		// Test
 		routers.Health(health)
@@ -84,6 +85,7 @@ func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
 		// Car
 		routers.CarType(carTypes, cfg)
 		routers.Gearbox(gearboxes, cfg)
+		routers.CarModel(carModels, cfg)
 	}
 
 	v2 := api.Group("/v2")
